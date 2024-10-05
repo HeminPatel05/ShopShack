ebb04b316c8921442914cff4a9c2912a4098773b
Hemin Patel <patel.hemin@northeastern.edu>
2024-10-05 01:33:51 -0400

    created a starter front end using react added pages

---
+++ client/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/Layouts/Layout";
+const About = () => {
+  return (
+    <Layout>
+      <h1> about us</h1>
+    </Layout>
+  );
+};
+
+export default About;
