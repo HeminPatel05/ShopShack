c265ae83626615294d5abc8c33aad31e1f9ccec5
Hemin Patel <patelhemin55@gmail.com>
2024-10-27 18:12:02 -0400

    made some changes to deploy site

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 //dotenv config
 dotenv.config();
@@ -20,6 +21,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -27,10 +29,8 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoutes);
 
 // rest API
-app.get("/", (req, res) => {
-  res.send({
-    message: "welcome to ecom app",
-  });
+app.use("*", function (req, res) {
+  res.sendFile(__dirname, "./client/build/index.html");
 });
 
 // PORT
